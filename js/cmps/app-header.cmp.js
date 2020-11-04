944392af432a92dda5c1af6499affedac5c2d5cd
Maya Malik <maya28892@gmail.com>
2020-11-04 11:22:41 +0200

    nav

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,11 +1,21 @@
 export default {
     template: `
-        <nav>
-            <router-link to="/" exact>Home</router-link>|
-            <router-link to="/book" exact>Books</router-link>|
-            <router-link to="/email" exact>eMail</router-link>|
-            <router-link to="/keep" exact>Keep</router-link>|
-            <router-link to="/about">About Us</router-link>
+        <nav class="main-nav flex align-center justify-space-between">
+            <router-link to="/" exact>Home</router-link>
+            <ul>
+                <li>
+                    <router-link to="/book" exact>Books</router-link>
+                </li>
+                <li>
+                    <router-link to="/email" exact>eMail</router-link>
+                </li>
+                <li>
+                    <router-link to="/keep" exact>Keep</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">About Us</router-link>
+                </li>
+            </ul>
         </nav>    
     `,
 
