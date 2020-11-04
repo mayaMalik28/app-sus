6ea2acc48d83c84a8cc96bdc450f0700ade36a74
Stav Partouche <stave1995@gmail.com>
2020-11-04 10:55:37 +0200

    more folders

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import homePage from './pages/home-page.cmp.js'
-import bookMain from './apps/book/book-main.cmp.js'
-import emailMain from './apps/email/email-main.cmp.js'
-import keepMain from './apps/keep/keep-main.cmp.js'
+import bookMain from './apps/book/pages/book-main.cmp.js'
+import emailMain from './apps/email/pages/email-main.cmp.js'
+import keepMain from './apps/keep/pages/keep-main.cmp.js'
 import aboutPage from './pages/about-page.cmp.js'
 
 const myRoutes = [
