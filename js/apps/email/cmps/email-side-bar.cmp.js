405b53bde0bb7dc6619db6a7cdf354bc20199578
Maya Malik <maya28892@gmail.com>
2020-11-07 15:21:45 +0200

    kjuhge

--- js/apps/email/cmps/email-side-bar.cmp.js
+++ js/apps/email/cmps/email-side-bar.cmp.js
@@ -7,10 +7,14 @@ export default {
         <aside class="sidebar flex-column align-center">
             <email-compose v-if="isCompose" @closeCompose="closeCompose" />
             <button class="compose-btn" @click="openCompose">compose <i class="fas fa-plus"></i></button>
-            <router-link class="category-btn" to="/email/inbox"><p @click="changeCategoryBy('isInbox')"><i class="fas fa-inbox"></i>Inbox</p></router-link>
+            <button class="category-btn" @click="changeCategoryBy('isInbox')"><i class="fas fa-inbox"></i>Inbox</button>
+            <button class="category-btn" @click="changeCategoryBy('isSent')"><i class="fas fa-share-square"></i>Sent</button>
+            <button class="category-btn" @click="changeCategoryBy('isStarred')"><i class="fas fa-star"></i>Starred</button>
+            <button class="category-btn"  @click="changeCategoryBy('isDraft')"><i class="fab fa-firstdraft"></i>Drafts</button> 
+            <!-- <router-link class="category-btn" to="/email/inbox" @click="changeCategoryBy('isInbox')"><i class="fas fa-inbox"></i>Inbox</router-link>
             <router-link class="category-btn" to="/email/sent"><p @click="changeCategoryBy('isSent')"><i class="fas fa-share-square"></i>Sent</p></router-link>
             <router-link class="category-btn" to="/email/starred"><p @click="changeCategoryBy('isStarred')"><i class="fas fa-star"></i>Starred</p></router-link>
-            <router-link class="category-btn" to="/email/drafts"><p @click="changeCategoryBy('isDraft')"><i class="fab fa-firstdraft"></i>Drafts</p></router-link> 
+            <router-link class="category-btn" to="/email/drafts"><p @click="changeCategoryBy('isDraft')"><i class="fab fa-firstdraft"></i>Drafts</p></router-link>  -->
         </aside>
         `,
     data() {
@@ -24,6 +28,10 @@ export default {
         changeCategoryBy(by) {
             console.log(by);
             emailService.setCurrCategory(by);
+            if (by === 'isInbox') this.$router.push('/email/inbox');
+            if (by === 'isSent') this.$router.push('/email/sent');
+            if (by === 'isStarred') this.$router.push('/email/starred');
+            if (by === 'isDraft') this.$router.push('/email/drafts');
         },
         openCompose() {
             this.isCompose = true
