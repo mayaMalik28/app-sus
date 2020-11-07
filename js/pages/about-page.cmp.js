46cdebd7e7f59042a9d2a05cb21925dd31ceb381
Stav Partouche <stave1995@gmail.com>
2020-11-07 16:57:24 +0200

    hjk

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -2,20 +2,17 @@ export default {
     template: `
     <section class="about-page">
         <h1>Our Team</h1>
-        <div class="person card">
-            <img class="person-img" scr=""/>
-            <div>
-                <h3 class="person header">Maya</h3>
+        <div class="person-card">
+            <img class="person-img" src="./js/assets/img/maya-img.jpeg"/>
+            <div class="person-txt">
+                <h3 class="person header">Maya Malik</h3>
                 <p class="person-content"></p>
             </div>
         </div>
-        <div class="person card" >
-        <img src="./js/assets/img/book.png" alt="">
-
-            
-            
-            <div>
-                <h3 class="person header">Stav</h3>
+        <div class="person-card" >
+        <img class= "person-img" src="./js/assets/img/stav-img.jpeg">
+            <div class="person-txt">
+                <h3 class="person header">Stav Partouche</h3>
                 <p class="person-content">Hello there! my name is Stav Partouche and 50% of this web site is my doing! 
                     with vast knowledge in front-end and back-end I can make any Website come to life.
                 </p>
