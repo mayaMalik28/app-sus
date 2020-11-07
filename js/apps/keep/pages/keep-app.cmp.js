f0eb67252fbae3a96df72c3ea44bfbc760788676
Stav Partouche <stave1995@gmail.com>
2020-11-07 19:46:24 +0200

    jk

--- js/apps/keep/pages/keep-app.cmp.js
+++ js/apps/keep/pages/keep-app.cmp.js
@@ -8,6 +8,9 @@ export default {
     name:'keepApp',
     template:`
     <section class="keep-app">
+        <div>
+            <input type="text" v-model="filterByText" placeholder="Search Note" />
+        </div>
         <keep-edit />
             <select class="keep-filter" v-model="filterBy">
                 <option>All</option>
@@ -24,7 +27,8 @@ export default {
         return {
             notes : null,
             pinnedNotes: null,
-            filterBy: 'All'
+            filterBy: 'All',
+            filterByText:''
         }
     },
     methods:{
@@ -33,11 +37,34 @@ export default {
     computed:{
         notesToShow(){
             if (this.filterBy === "All") return this.notes;
+            // const txt = this.filterByText
+            // const type = this.filterBy
+            //     if(type === "All"){
+            //         return this.notes.filter(note => {
+            //             note.info.title.toLowerCase().includes(txt) 
+            //         })
+            //     }else{
+            //         return this.notes.filter(note => {
+            //             (note.info.title.toLowerCase().includes(txt) &&
+            //             note.type === type)
+            //         })
+            //     }
+            
             return this.notes.filter(note => note.type === this.filterBy)
         },
         pinnedNotesToShow(){
-            if (this.filterBy === "All") return this.pinnedNotes;
-            return this.pinnedNotes.filter(note => note.type === this.filterBy)
+            if (this.filterBy === "All" && this.filterByText === '') return this.pinnedNotes;
+            const txt = this.filterByText
+            const type = this.filterBy
+            if(type ==='All'){
+                return this.pinnedNotes.filter(note => {
+                    console.log(txt);
+                    console.log(note.info.title.toLowerCase().includes(txt));
+                    note.info.title.toLowerCase().includes(txt)
+                    
+                })
+            }
+            // return this.pinnedNotes.filter(note => note.type === this.filterBy)
         }
     },
     components: {
