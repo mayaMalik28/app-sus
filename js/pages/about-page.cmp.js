681a8e7f8d45340bfa20d1ea8896c33775c31373
Stav Partouche <stave1995@gmail.com>
2020-11-07 16:30:40 +0200

    ghjk

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -3,14 +3,14 @@ export default {
     <section class="about-page">
         <h1>Our Team</h1>
         <div class="person card">
-            <img class="person-img" scr=""/>
+            <img class="person-img" src=""/>
             <div>
                 <h3 class="person header">Maya</h3>
                 <p class="person-content"></p>
             </div>
         </div>
         <div class="person card" >
-            
+            <img class="person-img" src="https://www.epicentrofestival.com/wp-content/uploads/2019/11/Owl-Drawing-Free-Clip-Art-Elephant-Motif-720x880.jpg"/>
             <div>
                 <h3 class="person header">Stav</h3>
                 <p class="person-content">Hello there! my name is Stav Partouche and 50% of this web site is my doing! 
